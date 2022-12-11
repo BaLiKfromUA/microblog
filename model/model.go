7c5d6fc567e977dce897468cb8111fb9ad1adaa0
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2022-12-11 13:37:07 +0200

    fix model serialization

--- model/model.go
+++ model/model.go
@@ -9,10 +9,10 @@ type PageToken string
 
 type Post struct {
 	Token     primitive.ObjectID `json:"-" bson:"_id,omitempty"`
-	Id        PostId             `json:"id,omitempty" pattern:"[A-Za-z0-9_\\-]+"`
-	Text      string
-	AuthorId  UserId       `json:"authorId,omitempty" pattern:"[0-9a-f]+"`
-	CreatedAt ISOTimestamp `json:"createdAt,omitempty" pattern:"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d{1,3})?Z"`
+	Id        PostId             `json:"id,omitempty" bson:"id" pattern:"[A-Za-z0-9_\\-]+"`
+	Text      string             `json:"text" bson:"text"`
+	AuthorId  UserId             `json:"authorId,omitempty" bson:"authorId" pattern:"[0-9a-f]+"`
+	CreatedAt ISOTimestamp       `json:"createdAt,omitempty" bson:"createdAt" pattern:"\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d{1,3})?Z"`
 }
 
 const EmptyPage = PageToken("none")
